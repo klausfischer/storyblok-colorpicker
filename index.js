987ef8a8c645701bab48d000a282a2a37a1a53c2
Klaus Fischer <klaus.fischer@netural.com>
2016-12-12 14:57:20 +0100

    use vue-color 0.0.4 (vue 1.x)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const styles = `<style>
+const styles = `
+	<style>
 	.cbox {
 		width: 15px;
 		height: 15px; 
@@ -10,30 +11,33 @@ const styles = `<style>
 const template = `
 	${styles}
 	<input v-model="model" class="uk-width-1-1" />
-	<span class="cbox" v-for="c in colors" v-bind:style="{ backgroundColor: c}" v-on:click="colorClicked"></span>
+	<sketch-picker :colors.sync="colors"></sketch-picker>
 	`;
 
 module.exports = {
+	props: ['model'],
+	components: {
+		'sketch-picker': function (resolve, reject) {
+			jQuery.getScript('https://cdn.rawgit.com/xiaokaike/vue-color/0.0.4/dist/vue-color.min.js', function() {
+				resolve(window.VueColor.Sketch);
+			});
+		}
+	},
 	template,
 	data () {
 		return {
 			model: "red",
-			colors: [
-				"#5E3AD9", "#30EAC0", "#A1FA42", "#B2655F", "#CCBF43"
-			]
-		}
-	},
-	watch: {
-		'model': {
-			handler (value) {
-				console.log('changed color', value);
-				this.$emit('changed-model', value);
+			colors: {
+				hex: "#1a5e25"
 			}
 		}
 	},
-	methods: {
-		colorClicked (event) {
-			this.model = event.target.style.backgroundColor;
+	watch: {
+		'colors': {
+			handler (value) {
+				this.$emit('changed-model', value.hex);
+			},
+      		deep: true
 		}
 	}
 };
