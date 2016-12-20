1386e23d30edd9eedaba00817b11ac6668ff742c
DominikAngerer <me@dominikangerer.com>
2016-12-20 10:28:13 +0100

    Create index.es5.js

---
+++ index.es5.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var template = '\n\t<sketch-picker :colors.sync="colors"></sketch-picker>\n\t';
+
+module.exports = {
+	props: ['model'],
+	components: {
+		'sketch-picker': function sketchPicker(resolve, reject) {
+			jQuery.getScript('https://cdn.rawgit.com/xiaokaike/vue-color/0.0.4/dist/vue-color.min.js', function () {
+				resolve(window.VueColor.Sketch);
+			});
+		}
+	},
+	template: template,
+	data: function data() {
+		return {
+			colors: {
+				hex: "#1a5e25"
+			}
+		};
+	},
+
+	watch: {
+		'colors': {
+			handler: function handler(value) {
+				this.$emit('changed-model', value.hex);
+			},
+
+			deep: true
+		}
+	}
+};
